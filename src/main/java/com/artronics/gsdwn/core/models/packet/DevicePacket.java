cb920a071b79c6b79bd576ebce88444310717a53
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-30 21:23:25 +0100

    add controllerId to devicePacket

--- src/main/java/com/artronics/gsdwn/core/models/packet/DevicePacket.java
+++ src/main/java/com/artronics/gsdwn/core/models/packet/DevicePacket.java
@@ -9,11 +9,13 @@ public class DevicePacket
     private final static Logger log = Logger.getLogger(DevicePacket.class);
 
     private final List<Integer> buffer;
+    private final Long controllerId;
     private final Long deviceId;
 
-    public DevicePacket(List<Integer> buffer, Long deviceId)
+    public DevicePacket(List<Integer> buffer,Long controllerId, Long deviceId)
     {
         this.buffer = buffer;
+        this.controllerId = controllerId;
         this.deviceId = deviceId;
     }
 
